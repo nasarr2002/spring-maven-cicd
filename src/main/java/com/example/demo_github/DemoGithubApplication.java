3f347d0e948207f8ed2c60e40ed4cac30497405e
nas <sarrndeyeaminata73@gmail.com>
2026-03-31 13:50:00 +0000

    Improve landing page UI

--- src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -2,23 +2,11 @@ package com.example.demo_github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @SpringBootApplication
 public class DemoGithubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoGithubApplication.class, args);
-	}
-
-}
-
-@RestController
-class HelloWorldController {
-    @GetMapping("/")
-    public String sayHello() {
-        return "Hello, World!";
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubApplication.class, args);
     }
 }
